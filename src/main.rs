c3b1112919d45d4374e74bd1ef5a8e4b44228962
i-m-aditya <prblmslvr.aditya@gmail.com>
2023-10-04 13:51:14 +0530

    HTTP server in rust

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+// Uncomment this block to pass the first stage
+use std::{
+    io::{BufRead, Write},
+    net::{TcpListener, TcpStream},
+};
+
+use anyhow::Error;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut reader = std::io::BufReader::new(&stream);
+    let mut request_line = String::new();
+
+    // Read the first line of the HTTP request (request line)
+    if reader.read_line(&mut request_line).is_ok() {
+        // Parse the request line to extract the path
+        let request_parts: Vec<&str> = request_line.trim().split(' ').collect();
+        if request_parts.len() >= 2 {
+            // let method = request_parts[0];
+            let mut path = request_parts[1];
+
+            match path {
+                "/" => {
+                    let response = "HTTP/1.1 200 OK\r\n\r\n";
+                    stream.write_all(response.as_bytes()).unwrap();
+                    stream.flush().unwrap();
+                }
+                p if p.starts_with("/echo") => {
+                    let echo_str = &path[6..];
+                    let response = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-length: {}\r\n\r\n{}",
+                        echo_str.len(),
+                        echo_str
+                    );
+                    stream.write_all(response.as_bytes()).unwrap();
+                    stream.flush().unwrap();
+                }
+                _ => {
+                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    stream.write_all(response.as_bytes()).unwrap();
+                    stream.flush().unwrap();
+                }
+            }
+
+            // println!("Method: {}", method);
+            // println!("Path: {}", echo_str);
+
+            // if path == "/" {
+            //     let response = "HTTP/1.1 200 OK\r\n\r\n";
+            //     stream.write_all(response.as_bytes()).unwrap();
+            //     stream.flush().unwrap();
+            // } else {
+            //     let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+            //     stream.write_all(response.as_bytes()).unwrap();
+            //     stream.flush().unwrap();
+            // }
+
+            // You can now use the 'path' variable in your application.
+        }
+    }
+
+    // Respond to the client (you would handle the request here)
+}
+
+fn main() -> Result<(), Error> {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
+
+    // Uncomment this block to pass the first stage
+    //
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("accepted new connection");
+                handle_client(stream);
+
+                // stream
+                //     .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                //     .unwrap()
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        };
+    }
+    Ok(())
+}
